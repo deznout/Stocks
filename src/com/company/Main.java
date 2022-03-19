f4d1c0ab1f3026731a025c041d8ba81696adb6df
Hoover Brent <hbrent@et-c2.kzn.21-school.ru>
2022-03-19 22:06:11 +0300

    try #2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,14 @@
 package com.company;
 
-import static com.company.StocksCount.Fn;
+import static com.company.StocksCount.*;
 import static java.lang.Math.pow;
 
 public class Main {
     public static void main(String[] args) {
         int N = 2;
         double a = 0.3, b = 0.4, r = 0.1, T = 110, S_0 = 100, B_0 = 10;
-//        Scanner in = new Scanner(System.in);
+        StocksCount stocks = new StocksCount();
+        //        Scanner in = new Scanner(System.in);
 //
 //        System.out.println("Input values N:");
 //        N = in.nextInt();
@@ -24,14 +25,18 @@ public class Main {
 //        b = in.nextDouble();
 //        System.out.println("Input values r:");
 //        r = in.nextDouble();
-        int n = 0;
-        //for (int n = 0; n < N; n++) {
-            double p = (r + a) / (a + b);
-            //double x = S_0; // gamma(T, r, N, n, a, b, p, S_0) * S_0 + betta(B_0, T, r, N, n, a, b, p, S_0) * B_0;
-            double summ = Fn(S_0, -a, b, N, T, p);
-            double C_n = summ * pow(1 + r, -N);
+        //int n = 0;
+        double C_n = 0;
+
+        double p = (r + a) / (a + b);
+        C_n = Fn(S_0, -a, b, N, T, p) * pow(1 + r, -N);
+        for (int n = 0; n < N + 1; n++) {
+            stocks.betta = betta_increase(B_0, T, r, N, n, -a, b, p, S_0);
+            stocks.gamma = gamma_increase(T, r, N, n, -a, b, p, S_0);
+            S_0 += S_0 * (1 + b);
+            //double x = S_0; //gamma(T, r, N, n, a, b, p, S_0) * S_0 + betta(B_0, T, r, N, n, a, b, p, S_0) * B_0;
+        }
         System.out.println(C_n);
-        //}
     }
 }
 
--- src/com/company/StocksCount.java
+++ src/com/company/StocksCount.java
@@ -4,15 +4,31 @@ import static java.lang.Math.max;
 import static java.lang.Math.pow;
 
 public class StocksCount {
+    double  betta;
+    double  gamma;
 
-    public static double	gamma(double T, double r, int N, int n, double a, double b, double p, double S)
+    public static double	gamma_increase(double T, double r, int N, int n, double a, double b, double p, double S)
+    {
+        double result = pow(1 + r, -(N - n)) *
+                (Fn(S * (1 + b), a, b, n, T, p) - Fn(S * (1 + a), b, a, n, T, p)) / (S * (b - a));
+        return result;
+    }
+
+    public static double	betta_increase(double B_0, double T, double r, int N, int n, double a, double b, double p, double S)
+    {
+        double result = Fn(S, a, b, n, T, p) / B_0 + pow(1 + r, -(N - n)) *
+                (Fn(S * (1 + b), a, b, n, T, p) - Fn(S * (1 + a) , b, a, n, T, p)) / (B_0 * (b - a));
+        return result;
+    }
+
+    public static double	gamma_reduce(double T, double r, int N, int n, double a, double b, double p, double S)
     {
         double result = pow(1 + r, -(N - n)) *
                 (Fn(S, a, b, n, T, p) - Fn(S, b, a, n, T, p)) / (S * (b - a));
         return result;
     }
 
-    public static double	betta(double B_0, double T, double r, int N, int n, double a, double b, double p, double S)
+    public static double	betta_reduce(double B_0, double T, double r, int N, int n, double a, double b, double p, double S)
     {
         double result = Fn(S, a, b, n, T, p) / B_0 + pow(1 + r, -(N - n)) *
                 (Fn(S, a, b, n, T, p) - Fn(S, b, a, n, T, p)) / (B_0 * (b - a));
